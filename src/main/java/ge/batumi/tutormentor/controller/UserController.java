cbf85e210ec4691be1ac7cb801f892e7e0effb5f
Bejo <luibejo@gmail.com>
2025-06-24 19:25:16 +0400

    added javadocs

--- src/main/java/ge/batumi/tutormentor/controller/UserController.java
+++ src/main/java/ge/batumi/tutormentor/controller/UserController.java
@@ -9,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller class to handle user related stuff.
+ *
+ * @author Luka Bezhanidze
+ * @version 0.1
+ * @since 24.06.2025
+ */
 @RestController
 @RequestMapping("${request.mapping.prefix}/users")
 @RequiredArgsConstructor
@@ -16,6 +23,11 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     * Get all users from database
+     *
+     * @return List of {@link UserDb} object.
+     */
     @GetMapping
     public List<UserDb> getAllUsers() {
         return userService.findAll();
